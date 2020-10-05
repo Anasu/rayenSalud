4a48194861ad2eb0a12974a09cd1b4606e76990a
anasu <misushima@gmail.com>
2020-10-05 17:05:58 -0300

    Se agrega navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar></navbar>
+
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  name:"Tutoriales",
+  components: {
+    Navbar,
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -23,7 +31,6 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
 
     &.router-link-exact-active {
       color: #42b983;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" class="navigation--color">
+    <b-navbar-brand :to="{name:'Home'}">Tutoriales</b-navbar-brand>
+  </b-navbar>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.navigation--color {
+  background-color: #6200EE;
+
+  a{
+    color: #FFF;
+    margin-left: 2rem;
+
+    &:visited {
+      color: #FFF;
+    }
+  }
+}
+</style>
